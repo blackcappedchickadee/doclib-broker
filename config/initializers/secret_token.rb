c28d37fae4f52e3537717a9e82ed33fb6ae4fdfe
Cloudchirp <cloudchirp.chirpmaster@gmail.com>
2012-06-21 16:55:24 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoclibBroker::Application.config.secret_token = 'f066dd3a2fb9f96f760f4b307107b5dd34b79ce697812a04b2305023b5e7f86f05a97325d00488df667e3d727e3e3f4401dec7ec1cac8e7f7c6450d0d8d68d07'
